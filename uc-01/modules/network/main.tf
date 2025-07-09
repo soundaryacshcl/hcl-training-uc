5b02bb18adde712d2469325c07f9ae88636e9e07
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 20:02:38 +0530

    Update main.tf

--- uc-01/modules/network/main.tf
+++ uc-01/modules/network/main.tf
@@ -1,5 +1,8 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "main-vpc"
   }
@@ -7,6 +10,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
   tags = {
     Name = "main-igw"
   }
