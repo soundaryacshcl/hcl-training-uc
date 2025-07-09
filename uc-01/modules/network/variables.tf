61ec3565f30a96bb08e41d62a5d4661a066cf8b0
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 20:01:59 +0530

    Update variables.tf

--- uc-01/modules/network/variables.tf
+++ uc-01/modules/network/variables.tf
@@ -1,4 +1,3 @@
-
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
@@ -6,13 +5,13 @@ variable "vpc_cidr_block" {
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDRs for public subnets"
+  description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Availability zones to deploy public subnets"
+  description = "List of availability zones"
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
