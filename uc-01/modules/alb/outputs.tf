818f17d1f62888cca6fc0b0571f5e1e349774641
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 20:08:50 +0530

    Update outputs.tf

--- uc-01/modules/alb/outputs.tf
+++ uc-01/modules/alb/outputs.tf
@@ -2,7 +2,9 @@ output "security_group_id" {
   value = aws_security_group.alb_sg.id
 }
 
+
 output "target_group_arns" {
+  description = "Map of target group ARNs for EC2 attachment"
   value = {
     app1 = aws_lb_target_group.app1.arn
     app2 = aws_lb_target_group.app2.arn
