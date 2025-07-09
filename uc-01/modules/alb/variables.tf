554606b39506922ad462ad2cf901b5680f1c2bb6
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 19:55:17 +0530

    Update variables.tf

--- uc-01/modules/alb/variables.tf
+++ uc-01/modules/alb/variables.tf
@@ -1,1 +1,10 @@
+variable "vpc_id" {
+  description = "VPC ID for the ALB and security group"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List of public subnet IDs for ALB"
+  type        = list(string)
+}
 
