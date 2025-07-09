b849e1fbbab3b39590b17695c58fc1084b718754
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 19:59:14 +0530

    Update outputs.tf

--- uc-01/modules/network/outputs.tf
+++ uc-01/modules/network/outputs.tf
@@ -1,7 +1,12 @@
+
+
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
+
