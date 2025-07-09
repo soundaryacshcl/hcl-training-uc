73475a7d9a731a9ae4a6805135279b64aa20b2d2
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 20:05:53 +0530

    Update main.tf

--- uc-01/main.tf
+++ uc-01/main.tf
@@ -8,10 +8,9 @@ module "alb" {
   subnets = module.network.public_subnet_ids
 }
 
-
 module "ec2" {
-  source = "./modules/ec2"
-  vpc_id = module.network.vpc_id
-  subnets = module.network.public_subnets
-  alb_sg_id = module.alb.security_group_id
+  source     = "./modules/ec2"
+  vpc_id     = module.network.vpc_id
+  subnets    = module.network.public_subnet_ids  # ✅ fixed name
+  alb_sg_id  = module.alb.security_group_id
 }
