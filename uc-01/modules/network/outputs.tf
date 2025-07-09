d2877eec8e21e46ef9e0e6d3e1798ced728e5d78
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 19:42:38 +0530

    Update outputs.tf

--- uc-01/modules/network/outputs.tf
+++ uc-01/modules/network/outputs.tf
@@ -2,10 +2,6 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  value = [
-    aws_subnet.subnet_a.id,
-    aws_subnet.subnet_b.id,
-    aws_subnet.subnet_c.id
-  ]
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
