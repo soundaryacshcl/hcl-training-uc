3056d2772cbb98481831dbad16f64414ec613257
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 21:07:03 +0530

    Update main.tf

--- uc-01/modules/ec2/main.tf
+++ uc-01/modules/ec2/main.tf
@@ -1,10 +1,10 @@
 locals {
   apps = {
     app1 = {
-      path     = "/app1"
+      path     = "/"
       html     = "Home Page"
       subnet   = var.subnets[0]
-      health   = "/app1/index.html"
+      health   = "/index.html"
     }
     app2 = {
       path     = "/image"
@@ -22,35 +22,19 @@ locals {
 }
 
 resource "aws_instance" "apps" {
-  for_each                     = local.apps
-  ami                          = "ami-05ffe3c48a9991133"
-  instance_type                = "t2.micro"
-  subnet_id                    = each.value.subnet
-  vpc_security_group_ids       = [var.alb_sg_id]
-  associate_public_ip_address  = true
+  for_each                    = local.apps
+  ami                         = "ami-05ffe3c48a9991133"
+  instance_type               = "t2.micro"
+  subnet_id                   = each.value.subnet
+  vpc_security_group_ids      = [var.alb_sg_id]
+  associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              mkdir -p /var/www/html${each.value.path}
+  user_data = templatefile("${path.module}/apache_setup.sh.tpl", {
+  path = each.value.path
+  html = each.value.html
+  name = each.key
+  })
 
-              cat <<EOT > /var/www/html${each.value.path}/index.html
-              <html><head><title>${each.key}</title></head>
-              <body><h1>${each.value.html}!</h1><p>(${each.key})</p></body></html>
-              EOT
-
-              echo "<VirtualHost *:80>
-                Alias ${each.value.path} /var/www/html${each.value.path}
-                <Directory /var/www/html${each.value.path}>
-                  Require all granted
-                </Directory>
-              </VirtualHost>" > /etc/httpd/conf.d/${each.key}.conf
-
-              rm -f /etc/httpd/conf.d/welcome.conf
-              systemctl start httpd
-              systemctl enable httpd
-              EOF
 
   tags = {
     Name = "${each.key}-instance"
