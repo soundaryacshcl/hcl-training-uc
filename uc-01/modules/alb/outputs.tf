07be86feee7a81b4b8043f034f9d1de48efe17a6
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-09 19:44:27 +0530

    Create outputs.tf

---
+++ uc-01/modules/alb/outputs.tf
@@ -0,0 +1,11 @@
+output "security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "target_group_arns" {
+  value = {
+    app1 = aws_lb_target_group.app1.arn
+    app2 = aws_lb_target_group.app2.arn
+    app3 = aws_lb_target_group.app3.arn
+  }
+}
