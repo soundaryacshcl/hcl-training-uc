15a9341e4e26b9b5b71235d5e8c94ad99a7c23b6
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-10 17:15:51 +0530

    Update variables.tf

--- UC-001.01/variables.tf
+++ UC-001.01/variables.tf
@@ -13,3 +13,13 @@ variable "public_subnets" {
 variable "private_subnets" {
   type = list(string)
 }
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for the EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "EC2 instance type"
+}
