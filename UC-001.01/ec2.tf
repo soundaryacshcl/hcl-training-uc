15a8084af8dabbe697c97d9d0b21c74425330673
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-10 17:20:32 +0530

    Update ec2.tf

--- UC-001.01/ec2.tf
+++ UC-001.01/ec2.tf
@@ -1,17 +1,15 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.5.0"
+  version = "4.3.0"
 
-  name = "my-ec2-instance"
-
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = module.vpc.public_subnets[0]
-
+  name           = "my-ec2-instance"
+  ami            = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name        = "modules-instance"
+    Name        = "my-ec2-instance"
     Environment = "dev"
   }
 }
