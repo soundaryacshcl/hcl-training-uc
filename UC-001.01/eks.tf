74ecdfd473d1dfeb006b0fcfc8ba7606eee1cd5f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-10 17:28:13 +0530

    Update eks.tf

--- UC-001.01/eks.tf
+++ UC-001.01/eks.tf
@@ -1,1 +1,27 @@
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.22.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
