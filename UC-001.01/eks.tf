9266857ea0e2f5ed15d0266a8bc6ac0eb5566949
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-10 18:16:00 +0530

    Update eks.tf

--- UC-001.01/eks.tf
+++ UC-001.01/eks.tf
@@ -1,30 +1,21 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.33.0"
-
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.31"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access           = true
-
-  cluster_addons = {
-    coredns                 = {}
-    kube-proxy              = {}
-    vpc-cni                 = {}
-    eks-pod-identity-agent  = {}
-  }
-
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+ source  = "terraform-aws-modules/eks/aws"
+ version = "20.8.4"
+ cluster_name    = "my-eks-cluster"
+ cluster_version = "1.29"
+ vpc_id     = module.vpc.vpc_id
+ subnet_ids = module.vpc.private_subnets
+ enable_irsa = true
+ eks_managed_node_groups = {
+   default = {
+     instance_types   = ["t2.medium"]
+     min_capacity     = 1
+     max_capacity     = 2
+     desired_capacity = 1
+   }
+ }
+ tags = {
+   Environment = "dev"
+   Terraform   = "true"
+ }
 }
