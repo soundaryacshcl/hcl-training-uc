d5df33c07e40bf29a6ebdd106b29f24f0ebbae13
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 14:52:49 +0530

    Update variables.tf

--- UC-02/variables.tf
+++ UC-02/variables.tf
@@ -1,12 +1,4 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-variable "rds_username" {
-  type      = string
-  sensitive = true
-}
 
-variable "rds_password" {
-  type      = string
-  sensitive = true
-}
