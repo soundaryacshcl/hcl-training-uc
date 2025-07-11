9823e82b4001d74c6c293262c71a8644325daf0f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 10:51:24 +0530

    Update variables.tf

--- UC-002/modules/rds/variables.tf
+++ UC-002/modules/rds/variables.tf
@@ -1,1 +1,18 @@
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "mydb"
+}
 
+variable "db_user" {
+  description = "Database username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+  default     = "password123"
+}
