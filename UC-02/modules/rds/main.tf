2c79b09ec46e545d588b8698cf227695adf4dd85
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 11:11:46 +0530

    Create main.tf

---
+++ UC-02/modules/rds/main.tf
@@ -0,0 +1,22 @@
+variable "subnet_ids" {}
+variable "sg_id" {}
+variable "db_subnet_group_name" {}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.subnet_ids
+  tags       = { Name = "RDS subnet group" }
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "password123"
+  db_subnet_group_name = aws_db_subnet_group.rds.name
+  vpc_security_group_ids = [var.sg_id]
+  skip_final_snapshot = true
+}
