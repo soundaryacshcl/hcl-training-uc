5bb053530dea9054e5034574d79692cb949909bf
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 12:21:03 +0530

    Create variables.tf

---
+++ UC-02/modules/rds/variables.tf
@@ -0,0 +1,10 @@
+variable "db_username" {
+ description = "RDS DB username"
+ type        = string
+ sensitive   = true
+}
+variable "db_password" {
+ description = "RDS DB password"
+ type        = string
+ sensitive   = true
+}
