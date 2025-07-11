e97aa4ef780fd148a6f323f2bd96df99fc8c4663
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 17:21:21 +0530

    Update main.tf

--- UC-02/modules/ec2/main.tf
+++ UC-02/modules/ec2/main.tf
@@ -4,7 +4,7 @@ variable "instance_count" {}
 
 resource "aws_instance" "web" {
   count         = var.instance_count
-  ami           = "ami-05ffe3c48a9991133"
+  ami           = "var.ami_id"
   instance_type = "t2.micro"
   subnet_id     = var.subnet_ids[count.index]
   vpc_security_group_ids = [var.sg_id]
