184837792f142e6269785298c948cdcf95626e01
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 15:24:28 +0530

    Update backend.tf

--- UC-02/backend.tf
+++ UC-02/backend.tf
@@ -1,11 +1,8 @@
-
 terraform {
   backend "s3" {
-    bucket         = "test-s3-bucket-uc-02"
-    key            = "usecase2/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket = "test-s3-bucket-uc-02"
+    key    = "usecase2/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
   }
 }
-
