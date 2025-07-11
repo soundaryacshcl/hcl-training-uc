1245ab5bd3321ba3e137292fb8d11fab8b28e494
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 11:30:06 +0530

    Update main.tf

--- UC-02/modules/rds/main.tf
+++ UC-02/modules/rds/main.tf
@@ -8,15 +8,18 @@ resource "aws_db_subnet_group" "rds" {
   tags       = { Name = "RDS subnet group" }
 }
 
+
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "admin"
-  password             = "password123"
-  db_subnet_group_name = aws_db_subnet_group.rds.name
-  vpc_security_group_ids = [var.sg_id]
-  skip_final_snapshot = true
+ identifier             = "mydb"
+ allocated_storage      = 20
+ engine                 = "mysql"
+ engine_version         = "8.0"
+ instance_class         = "db.t3.micro"
+ username               = "admin"
+ password               = "password123"
+ db_subnet_group_name   = aws_db_subnet_group.rds.name
+ vpc_security_group_ids = [var.sg_id]
+ skip_final_snapshot    = true
 }
+
+
