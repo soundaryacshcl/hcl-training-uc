d903f8459715847cd4bb5d405e6ae0cf906706dd
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 10:21:39 +0530

    Create main.tf

---
+++ UC-002/modules/vpc/main.tf
@@ -0,0 +1,44 @@
+# modules/vpc/main.tf
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = { Name = "custom-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "custom-igw" }
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-${count.index}" }
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+  tags = { Name = "private-subnet-${count.index}" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "public-rt" }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
