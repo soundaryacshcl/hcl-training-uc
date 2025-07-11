d0a5cd03afcc071e56971ebb781c0677ed4dc29f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 11:02:32 +0530

    Create main.tf

---
+++ UC-02/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "sg" {
+  source = "./modules/sg"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "ec2" {
+  source          = "./modules/ec2"
+  subnet_ids      = module.vpc.public_subnet_ids
+  sg_id           = module.sg.web_sg_id
+  instance_count  = 2
+}
+
+module "rds" {
+  source               = "./modules/rds"
+  subnet_ids           = module.vpc.private_subnet_ids
+  sg_id                = module.sg.rds_sg_id
+  db_subnet_group_name = module.vpc.db_subnet_group
+}
