70296026bca772d769b22e6bf4b95da628bb0a29
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 10:50:25 +0530

    Update variables.tf

--- UC-002/variables.tf
+++ UC-002/variables.tf
@@ -4,29 +4,7 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
 
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
-}
-
-variable "azs" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
-}
 
 variable "my_ip" {
   description = "Your IP address for SSH access"
@@ -40,21 +18,4 @@ variable "ami_id" {
   default     = "ami-05ffe3c48a9991133"
 }
 
-variable "db_name" {
-  description = "Database name"
-  type        = string
-  default     = "mydb"
-}
 
-variable "db_user" {
-  description = "Database username"
-  type        = string
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "Database password"
-  type        = string
-  sensitive   = true
-  default     = "password123"
-}
