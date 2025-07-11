3926465e8cc641f4f005223f46b9cfc528ed2c9f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 12:29:39 +0530

    Update main.tf

--- UC-02/modules/rds/main.tf
+++ UC-02/modules/rds/main.tf
@@ -15,8 +15,8 @@ resource "aws_db_instance" "mysql" {
  engine                 = "mysql"
  engine_version         = "8.0"
  instance_class         = "db.t3.micro"
- username               = "var.RDS_USERNAME"
- password               = "var.RDS_PASSWORD"
+ username               = "var.db.username"
+ password               = "var.db.password"
  db_subnet_group_name   = aws_db_subnet_group.rds.name
  vpc_security_group_ids = [var.sg_id]
  skip_final_snapshot    = true
