451621d1ae6267b76ac94202869d8556d08812ba
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 11:10:35 +0530

    Create main.tf

---
+++ UC-02/modules/ec2/main.tf
@@ -0,0 +1,24 @@
+variable "subnet_ids" {}
+variable "sg_id" {}
+variable "instance_count" {}
+
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_ids[count.index]
+  vpc_security_group_ids = [var.sg_id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from Web Server ${count.index + 1}</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "WebServer-${count.index + 1}"
+  }
+}
