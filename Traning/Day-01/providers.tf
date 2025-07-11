ada217c1e8f0b4df3b17e80cd660fa4f0ef917b8
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 16:26:39 +0530

    Create providers.tf

---
+++ Traning/Day-01/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95.0, < 6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
