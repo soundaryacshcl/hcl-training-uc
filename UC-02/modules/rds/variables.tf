d918a386afde63987b7e0318357cc65a3948ccc8
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-11 14:33:41 +0530

    Update variables.tf

--- UC-02/modules/rds/variables.tf
+++ UC-02/modules/rds/variables.tf
@@ -1,10 +1,9 @@
-variable "db_username" {
- description = "RDS DB username"
- type        = string
- sensitive   = true
+variable "rds_username" {
+  type      = string
+  sensitive = true
 }
-variable "db_password" {
- description = "RDS DB password"
- type        = string
- sensitive   = true
+
+variable "rds_password" {
+  type      = string
+  sensitive = true
 }
